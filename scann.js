63cdba2ef6dc5ec911df01afa408bf183d40f83c
Julian Schmidt <info@julianb.de>
2021-11-09 15:32:03 +0100

    When found a dependency add the version to result
    @2alextill thank you for your hint and your improvement.

--- scann.js
+++ scann.js
@@ -1,20 +1,27 @@
-'use strict';
+"use strict";
 
-const { exec } = require('child_process');
+const { exec } = require("child_process");
+const path = require("path");
 
-const FgGreen = '\x1b[32m';
-const FgRed = '\x1b[31m';
+const FgGreen = "\x1b[32m";
+const FgRed = "\x1b[31m";
 
 let args = {
   path: [],
   dependencies: [],
 };
 process.argv.slice(2).forEach((item) => {
-  const newArg = item.split('=');
+  const newArg = item.split("=");
   args = {
     ...args,
-    [newArg[0]]: newArg[1].split(','),
+    [newArg[0]]: newArg[1].split(","),
   };
+  if (args.p) {
+    args.path = args.p;
+  }
+  if (args.d) {
+    args.dependencies = args.d;
+  }
 });
 
 const recursiveSearch = (obj, searchKey, results = []) => {
@@ -22,9 +29,10 @@ const recursiveSearch = (obj, searchKey, results = []) => {
   Object.keys(obj).forEach((key) => {
     const value = obj[key];
     if (value.endsWith(searchKey)) {
-      r.push(value);
+      const pjson = require(`${value}${path.sep}package.json`);
+      r.push(`${searchKey}: ${pjson.version}`);
     }
-    if (typeof value === 'object') {
+    if (typeof value === "object") {
       recursiveSearch(value, searchKey, r);
     }
   });
@@ -36,11 +44,11 @@ args.path.forEach((itemPath) => {
   console.info(`run in ${itemPath}`);
 
   exec(`npm list --parseable`, (erro, stdout, stderr) => {
-    const npmList = stdout.split('\n');
+    const npmList = stdout.split("\n");
     args.dependencies.forEach((dep) => {
       const found = recursiveSearch(npmList, dep, []);
       if (found.length > 0) {
-        console.error(FgRed, 'found dependency', found);
+        console.error(FgRed, "found dependency", found);
       } else {
         console.log(FgGreen, `${dep}: not found`);
       }
